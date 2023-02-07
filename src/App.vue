ae6978fb758ca8a9b3a566881c99ff173dd7395d
Kuldar Nei <kuldarnei@gmail.com>
2023-02-07 09:55:54 +0200

    Bootstrap import

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
+  <div>
+
   <div id="app">
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </nav>
     <router-view/>
+  </div>
+
   </div>
 </template>
 
