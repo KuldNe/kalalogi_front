f44f4e209bdeea541a1dcc2e584e3b8fd87fcba1
Marii <mtelliskivi@gmail.com>
2023-02-07 14:18:22 +0200

    commit

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
           <svg class="bi me-2" width="40" height="32">
             <use xlink:href="#bootstrap"></use>
           </svg>
-          <span class="fs-4">Sidebar</span>
+          <span class="fs-4">Kalalogi</span>
         </a>
         <hr>
         <ul class="nav nav-pills flex-column mb-auto">
@@ -16,7 +16,7 @@
               <svg class="bi me-2" width="16" height="16">
                 <use xlink:href="#home"></use>
               </svg>
-              Home
+              Püügiandmed
             </a>
           </li>
           <li>
@@ -24,45 +24,21 @@
               <svg class="bi me-2" width="16" height="16">
                 <use xlink:href="#speedometer2"></use>
               </svg>
-              Dashboard
+              Logi sisse/ registreeru
             </a>
           </li>
-          <li>
-            <a href="#" class="nav-link text-white">
-              <svg class="bi me-2" width="16" height="16">
-                <use xlink:href="#table"></use>
-              </svg>
-              Orders
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link text-white">
-              <svg class="bi me-2" width="16" height="16">
-                <use xlink:href="#grid"></use>
-              </svg>
-              Products
-            </a>
-          </li>
-          <li>
-            <a href="#" class="nav-link text-white">
-              <svg class="bi me-2" width="16" height="16">
-                <use xlink:href="#people-circle"></use>
-              </svg>
-              Customers
-            </a>
-          </li>
+
         </ul>
         <hr>
-        <div class="dropdown">
+        <div class="dropdown"> Filtreeri
           <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
              id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-            <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-            <strong>mdo</strong>
           </a>
+
           <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-            <li><a class="dropdown-item" href="#">New project...</a></li>
-            <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><a class="dropdown-item" href="#">Kuupäev</a></li>
+            <li><a class="dropdown-item" href="#">Püügikoht</a></li>
+            <li><a class="dropdown-item" href="#">Kalaliik</a></li>
             <li>
               <hr class="dropdown-divider">
             </li>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap/dist/js/bootstrap.js"
 
+
 Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
 
