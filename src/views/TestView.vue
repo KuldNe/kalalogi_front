3ebe6c1d32295d93227d2f6f1939ca307662171c
pbernotas <priit.bernotas@gmail.com>
2023-02-07 15:48:24 +0200

    Catchdetails progress

--- src/views/TestView.vue
+++ src/views/TestView.vue
@@ -1,43 +1,36 @@
 <template>
-<div>
-  <table class="table table-dark table-striped col-2">
-    <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-    </thead>
-    <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-    </tbody>
-  </table>
-</div>
+  <CatchDetails/>
 </template>
 
 <script>
+
+import CatchDetails from "@/components/CatchDetails.vue";
+
 export default {
-  name: "TestView"
+  name: "TestView",
+  components: {CatchDetails},
+  data: function () {
+    return {
+      catches: {
+        species: [
+          {
+            speciesName:''
+          },
+        ],
+        length: 0,
+        weight: 0,
+        catchDate: '10-09-22',
+        waterbody: [
+          {
+            waterbodyName: ''
+          }
+        ],
+        userComment: '',
+        wasReleased: true,
+        picture: ''
+      }
+    }
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
