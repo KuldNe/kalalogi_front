627cf4e0d87f3df7e9ae04f6d72e25846c3eed94
pbernotas <priit.bernotas@gmail.com>
2023-02-07 09:37:03 +0200

    Algus

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import testView from "@/views/TestView.vue";
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/test',
+    name: 'test',
+    component: testView
+  },
   {
     path: '/',
     name: 'home',
